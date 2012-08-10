9254bbd6f3b778d95e119a947c4662f1f9d53e04
beepscore <support@beepscore.com>
2012-08-09 21:12:03 -0700

    Add frameworks GLKit, OpenGLES, QuartzCore. In EEAppDelegate adopt protocols.

--- ExampleEngine/EEAppDelegate.h
+++ ExampleEngine/EEAppDelegate.h
@@ -7,8 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
 
-@interface EEAppDelegate : UIResponder <UIApplicationDelegate>
+@interface EEAppDelegate : UIResponder
+    <UIApplicationDelegate, GLKViewDelegate, GLKViewControllerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
