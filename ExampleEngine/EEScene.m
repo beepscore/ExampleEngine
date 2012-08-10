e6f5aa36deb39b98e0ce44e19f1e9607722a802b
beepscore <support@beepscore.com>
2012-08-09 23:41:31 -0700

    In EEScene render, draw triangle.

--- ExampleEngine/EEScene.m
+++ ExampleEngine/EEScene.m
@@ -12,7 +12,7 @@
 
 
 -(void)update {
-   // NSLog(@"in EEScene's update"); 
+    // NSLog(@"in EEScene's update");
 }
 
 -(void)render {
@@ -20,6 +20,24 @@
     // r,g,b,alpha
     glClearColor(self.clearColor.r, self.clearColor.g, self.clearColor.b, self.clearColor.a);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    // triangle about origin
+    float vertices[] = {-1, -1, 1, -1, 0,  1};
+    
+    // allocating effect each time through the loop is very inefficient
+    // GLKBaseEffect uses OpenGL ES 2 shaders to mimic OpenGL ES 1.1
+    GLKBaseEffect *effect = [[GLKBaseEffect alloc] init];
+    [effect prepareToDraw];
+    
+    // tell shader to use vertex position data
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    
+    // second parameter vertex position is 2 dimensional
+    glVertexAttribPointer(GLKVertexAttribPosition, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+    // draw the triangle
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // clean up
+    glDisableVertexAttribArray(GLKVertexAttribPosition);
 }
 
 @end
