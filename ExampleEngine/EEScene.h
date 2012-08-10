f461f4a8cd149251f976d6977d0730726157d51b
beepscore <support@beepscore.com>
2012-08-09 22:20:34 -0700

    Add group Scenes and EEScene.

---
+++ ExampleEngine/EEScene.h
@@ -0,0 +1,13 @@
+//
+//  EEScene.h
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/9/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EEScene : NSObject
+
+@end
---
+++ ExampleEngine/EEScene.m
@@ -0,0 +1,13 @@
+//
+//  EEScene.m
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/9/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "EEScene.h"
+
+@implementation EEScene
+
+@end
