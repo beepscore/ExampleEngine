50e5ef66b135ba773f9c2c0f181a0c2d61a0f37a
beepscore <support@beepscore.com>
2012-08-11 15:19:31 -0700

    Add class EEShape.

---
+++ ExampleEngine/EEShape.h
@@ -0,0 +1,13 @@
+//
+//  EEShape.h
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EEShape : NSObject
+
+@end
---
+++ ExampleEngine/EEShape.m
@@ -0,0 +1,13 @@
+//
+//  EEShape.m
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "EEShape.h"
+
+@implementation EEShape
+
+@end
