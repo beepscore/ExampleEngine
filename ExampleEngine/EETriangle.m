bafc9b46e7f48a74d69dd22239207eb40dd3b2ad
beepscore <support@beepscore.com>
2012-08-11 16:16:02 -0700

    Add EETriangle as a subclass of EEShape.

---
+++ ExampleEngine/EETriangle.m
@@ -0,0 +1,17 @@
+//
+//  EETriangle.m
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "EETriangle.h"
+
+@implementation EETriangle
+
+- (int)numVertices {
+    return 3;
+}
+
+@end
