38c9c8afffe81ea605b6e7b5c0fa598fa8c8c227
beepscore <support@beepscore.com>
2012-08-11 19:48:22 -0700

    Add class RectangleScene.

---
+++ ExampleEngine/RectangleScene.h
@@ -0,0 +1,13 @@
+//
+//  RectangleScene.h
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "EEScene.h"
+
+@interface RectangleScene : EEScene
+
+@end
---
+++ ExampleEngine/RectangleScene.m
@@ -0,0 +1,13 @@
+//
+//  RectangleScene.m
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "RectangleScene.h"
+
+@implementation RectangleScene
+
+@end
