fe64cc68dcf1891d1136a48734584b56898660ad
beepscore <support@beepscore.com>
2012-08-11 22:17:44 -0700

    Add class EERegularPolygon.

---
+++ ExampleEngine/EERegularPolygon.m
@@ -0,0 +1,13 @@
+//
+//  EERegularPolygon.m
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "EERegularPolygon.h"
+
+@implementation EERegularPolygon
+
+@end
