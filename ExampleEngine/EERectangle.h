bc9a75d8f987363492ebf60adddbec7e534ee952
beepscore <support@beepscore.com>
2012-08-11 18:58:56 -0700

    Add class EERectangle.

---
+++ ExampleEngine/EERectangle.h
@@ -0,0 +1,13 @@
+//
+//  EERectangle.h
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "EEShape.h"
+
+@interface EERectangle : EEShape
+
+@end
---
+++ ExampleEngine/EERectangle.m
@@ -0,0 +1,18 @@
+//
+//  EERectangle.m
+//  ExampleEngine
+//
+//  Created by Steve Baker on 8/11/12.
+//  Copyright (c) 2012 Steve Baker. All rights reserved.
+//
+
+#import "EERectangle.h"
+
+@implementation EERectangle
+
+
+- (int)numVertices {
+    return 4;
+}
+
+@end
